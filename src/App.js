84230301717960b787f709a93830fc0f9358a381
mcmillanj <mcmillanj0106@yahoo.com>
2024-05-18 19:36:24 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Field from './components/field'
 import Translate from './components/translate'
 import Languages from './components/languages'
 
+//Language Translator
 export default function App() {
   const [language, setLanguage] = useState('es')
   const [text, setText] = useState('')
